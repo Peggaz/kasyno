30e47e60ef18bb2b6dc2a992e342ca8fef3350e2
Jakub Nowocień <jakub_nowocien47@wp.pl>
2022-06-16 12:05:08 +0200

    init commit

---
+++ kasyno gra/kasyno gra/User.cpp
@@ -0,0 +1,13 @@
+#include "User.h"
+User::User()
+{
+	
+}
+void User::SetSaldo(int saldo)
+{
+	m_saldo = saldo;
+}
+int User::GetSaldo()
+{
+	return m_saldo;
+}
---
+++ kasyno gra/kasyno gra/User.h
@@ -0,0 +1,14 @@
+#pragma once
+class User
+{
+private:
+	int m_saldo;
+	int m_id;
+public:
+	User();
+	void SetSaldo(int saldo);
+	int GetSaldo();
+};
+
+
+
